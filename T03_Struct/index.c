166fdd764671c30b41f99b27de6100632f78564b
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-10-10 17:49:55 +0700

    Add Delete Module

--- T03_Struct/index.c
+++ T03_Struct/index.c
@@ -17,12 +17,12 @@ struct Mahasiswa
 void act_create(struct Mahasiswa mhs);
 void act_read(char npm[16]);
 void act_update();
-// int act_delete();
+void act_delete();
 
 void data_create();
 void data_read();
 void data_update();
-// void data_delete();
+void data_delete();
 
 
 int main()
@@ -43,7 +43,7 @@ int main()
             case 1: data_create(); break;
             case 2: data_read(); break;
             case 3: data_update(); break;
-            // case 'D': data_delete(); break;
+            case 4: data_delete(); break;
             case 5: exit(1); break;
             
             default:
@@ -100,6 +100,7 @@ int main()
         sqlite3_bind_int(final_query, 6, mhs.semester);
         sqlite3_bind_int(final_query, 7, mhs.usia);
         sqlite3_step(final_query);
+        sqlite3_close_v2(db);
     }
 
 
@@ -190,7 +191,7 @@ int main()
             //     printf("%16s|%32s|%8i|%32s|%32s|%8i|%4i\n", sqlite3_column_text(final_query, 0), sqlite3_column_text(final_query, 1), sqlite3_column_int(final_query, 2), sqlite3_column_text(final_query, 3), sqlite3_column_text(final_query, 4), sqlite3_column_int(final_query, 5), sqlite3_column_int(final_query, 6));
             // }
         }
-        
+        sqlite3_close_v2(db);
     }
 
 
@@ -201,7 +202,7 @@ int main()
         char npm[16], str[32];
         struct Mahasiswa mhs;
 
-        printf("NPM      : "); scanf("%s", mhs.npm);
+        printf("\nNPM      : "); scanf("%s", mhs.npm);
         printf("Nama     : "); scanf(" %[^\n]", mhs.nama);
         printf("Angkatan : "); scanf("%i", &mhs.angkatan);
         printf("Jurusan  : "); scanf(" %[^\n]", mhs.jurusan);
@@ -213,17 +214,6 @@ int main()
         printf("\nData berhasil diperbarui!\n");
         printf("==============================================\n\n");
     }
-
-    static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
-   fprintf(stderr, "%s: ", (const char*)data);
-   
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
 
     void act_update(char npm[16], struct Mahasiswa mhs) {
         sqlite3 *db;
@@ -372,4 +362,93 @@ int main()
         
         sqlite3_close_v2(db);
         
+    }
+
+
+// DELETE DATA
+
+    void data_delete() {
+        int i;
+        char npm[16];
+
+        printf("Opsi:\n");
+        printf("    [1] Hapus Semua Data\n");
+        printf("    [2] Hapus Berdasarkan NPM\n");
+        printf("\nPilihan Anda : "); scanf("%i", &i);
+        printf("==============================================\n\n");
+
+        switch (i)
+        {
+        case 1:
+            act_delete("all");
+            break;
+
+        case 2:
+            printf("Masukkan NPM : "); scanf("%s", npm);
+            printf("==============================================\n\n");
+            act_delete(npm);
+            break;
+        
+        default:
+            printf("Pilihan tidak tersedia!\n");
+            break;
+        }
+
+        printf("==============================================\n\n");
+    }
+
+    void act_delete (char npm[16]) {
+        sqlite3 *db;
+        sqlite3_stmt *final_query;
+        char *err_msg = 0;
+        char nama[32];
+        int i = 1;
+        int yakin;
+
+        int rc = sqlite3_open("db_mhs.db", &db);
+
+        if (rc != SQLITE_OK)
+        {
+            fprintf(stderr, "Can\'t load database: %s", sqlite3_errmsg(db));
+            sqlite3_close(db);
+        }
+
+        if (npm == "all")
+        {
+            char *sql_a = "SELECT COUNT(*) AS jumlah FROM Mahasiswa";
+            sqlite3_prepare_v2(db, sql_a, strlen(sql_a), &final_query, NULL);
+            sqlite3_step(final_query);
+            printf("\nAnda yakin ingin menghapus sebanyak %i data? Tekan [1] untuk perintah \'Ya\' : ", sqlite3_column_int(final_query, 0)); scanf("%i", &yakin);
+            
+            if (yakin == 1) {
+                char *sql_b = "DELETE FROM Mahasiswa";
+                sqlite3_prepare_v2(db, sql_b, strlen(sql_b), &final_query, NULL);
+                sqlite3_step(final_query);
+                printf("\nSemua data telah dihapus!\n");
+            } else {
+                printf("\nTidak ada data yang dihapus.\n");
+            }
+            
+            
+        }
+        else
+        {
+            char *sql_c = "SELECT * FROM Mahasiswa WHERE npm = ?";
+            sqlite3_prepare_v2(db, sql_c, strlen(sql_c), &final_query, NULL);
+            sqlite3_bind_text(final_query, 1, npm, strlen(npm), NULL);
+            sqlite3_step(final_query);
+            strcpy(nama, sqlite3_column_text(final_query, 1));
+            printf("Anda yakin ingin data dari %s dengan NPM %s? Tekan [1] untuk perintah \'Ya\' : ", nama, npm); scanf("%i", &yakin);
+
+            if (yakin == 1) {
+                char *sql_d = "DELETE FROM Mahasiswa WHERE npm = ?";
+                sqlite3_prepare_v2(db, sql_d, strlen(sql_d), &final_query, NULL);
+                sqlite3_bind_text(final_query, 1, npm, strlen(npm), NULL);
+                sqlite3_step(final_query);
+                printf("\nData dari %s dengan NPM %s telah dihapus!\n", nama, npm);
+            } else {
+                printf("\nTidak ada data yang dihapus.\n");
+            }
+        }
+        sqlite3_close_v2(db);
     }
