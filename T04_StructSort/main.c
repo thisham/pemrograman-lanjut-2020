de4e39db3b2810cb276c6a328322841cd9369620
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-10-23 01:37:53 +0700

    Add Selection Sort and Fixing Some Errors

--- T04_StructSort/main.c
+++ T04_StructSort/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /**
  * Hamdan Yuwafi Mastu Wijaya (NPM. 19081010092)
- * Judul        : CRUD-C with bubble sort
+ * Judul        : CRUD-C with bubble sort and selection sort
  * GitHub URL   : https://github.com/thisham/pemrograman-lanjut-2020/tree/master/T04_StructSort
  * Note         : Source code ini ditulis di Linux Ubuntu 18.04 dan dikompilasi dengan GCC for Linux.
  */
@@ -23,12 +24,14 @@ typedef struct
 int menu();
 void rd_data();
 int rd_menu();
+void bubble_sort(Students stu[], int length);
+void select_sort(Students stu[], int length);
 int compareNPM(const void *pa, const void *pb);
 
 int main(int argc, char const *argv[])
 {
     char npm[16];
-    int found = 0; int d = 0;
+    bool found; int d = 0;
     int opt, max, data = 0;
     printf("Insert max amount of data : "); scanf("%d", &max); 
     Students stu[max], srt_stu[max], tmp_stu;
@@ -41,13 +44,13 @@ int main(int argc, char const *argv[])
                 printf("Used data storage in array: %d / %d\n\n", data, max);
                 if (data < max) {
                     data = data + 1;
-                    printf("NPM        : "); scanf("%s", stu[data].npm);
-                    printf("Name       : "); scanf(" %[^\n]", stu[data].name);
-                    printf("Generation : "); scanf("%d", &stu[data].generation);
-                    printf("Major      : "); scanf(" %[^\n]", stu[data].majors);
-                    printf("Faculty    : "); scanf(" %[^\n]", stu[data].faculty);
-                    printf("Semester   : "); scanf("%d", &stu[data].semester);
-                    printf("Age        : "); scanf("%d", &stu[data].age);
+                    printf("NPM        : "); scanf("%s", stu[data - 1].npm);
+                    printf("Name       : "); scanf(" %[^\n]", stu[data - 1].name);
+                    printf("Generation : "); scanf("%d", &stu[data - 1].generation);
+                    printf("Major      : "); scanf(" %[^\n]", stu[data - 1].majors);
+                    printf("Faculty    : "); scanf(" %[^\n]", stu[data - 1].faculty);
+                    printf("Semester   : "); scanf("%d", &stu[data - 1].semester);
+                    printf("Age        : "); scanf("%d", &stu[data - 1].age);
                 } else {
                     printf("Error: Out of max amount of struct!\n");
                 }
@@ -59,53 +62,40 @@ int main(int argc, char const *argv[])
                     int a = rd_menu();
                     printf("===================================================\n\n");
                     switch (a) {
-                        case 1:
-                            for (int i = 1; i <= data; i++) {
-                                printf("Data number %i : \n", i);
+                        case 1: // returns all data
+                            for (int i = 0; i < data; i++) {
+                                printf("Data number %i : \n", i + 1);
                                 rd_data(stu[i]);
                                 printf("\n");
                             }
                             break;
 
-                        case 2:
+                        case 2: // returns data on selected npm
                             printf("Search NPM : "); scanf("%s", npm);
                             printf("\n");
-                            for (int i = 1; i <= data; i++) {
-                                if (strcmp(stu[i].npm, npm) == 0) {
-                                    found = found + 1;
-                                    d = i;
+                            for (int i = 0; i < data; i++) {
+                                // printf("%s == %s", stu[i].npm, npm);
+                                if (!strncmp(stu[i].npm, npm, sizeof(npm))) {
+                                    rd_data(stu[i]);
+                                    found = true;
+                                } else {
+                                    found = false;
                                 }
-                                
                             }
 
-                            if (found > 0) {
-                                rd_data(stu[d]);
-                            } else {
+                            // printf("%d\n", found);
+
+                            if (!found) {
                                 printf("Data not found!\n");
                             }
                             break;
 
-                        case 3:
-                            for (int i = 0; i < data; i++) {
-                                srt_stu[i] = stu[i + 1];
-                            }
+                        case 3: // returns bubble-sorted data by NPM (ASC)
+                            bubble_sort(stu, data);
+                            break;
 
-                            for (int i = 0; i < data; i++) {
-                                for (int j = 1; j < data; j++) {
-                                    if (strcmp(srt_stu[j-1].npm, srt_stu[j].npm) > 0) {
-                                        tmp_stu = srt_stu[j-1];
-                                        srt_stu[j-1] = srt_stu[j];
-                                        srt_stu[j] = tmp_stu;
-                                    }
-                                    
-                                }
-                            }
-                            
-                            for (int i = 0; i < data; i++) {
-                                printf("Data number %i :\n", i+1);
-                                rd_data(srt_stu[i]);
-                                printf("\n");
-                            }
+                        case 4: // returns selection-sorted data by NPM (ASC)
+                            select_sort(stu, data);
                             break;
 
                         default:
@@ -120,14 +110,15 @@ int main(int argc, char const *argv[])
             case 3: // update
                 printf("Search NPM : "); scanf("%s", npm);
                 printf("\n");
-                for (int i = 1; i <= data; i++) {
-                    if (strcmp(stu[i].npm, npm) == 0) {
-                        found = found + 1;
-                        d = i;
+                for (int i = 0; i < data; i++) {
+                    if (!strncmp(stu[i].npm, npm, sizeof(npm))) {
+                        found = true;
+                    } else {
+                        found = false;
                     }
                 }
 
-                if (found > 0) {
+                if (found) {
                     printf("Note: Please don\'t leave this fields blank. Repeat to keep it unchanged.\n");
                     printf("\tNPM        : (%s)\n", stu[d].npm);
                     printf("\tName       : (%s) => ", stu[d].name); scanf(" %[^\n]", stu[d].name);
@@ -146,22 +137,20 @@ int main(int argc, char const *argv[])
             case 4: // delete
                 printf("Search NPM : "); scanf("%s", npm);
                 printf("\n");
-                for (int i = 1; i <= data; i++) {
-                    if (strcmp(stu[i].npm, npm) == 0) {
-                        found = found + 1;
-                        d = i;
+                for (int i = 0; i < data; i++) {
+                    if (!strncmp(stu[i].npm, npm, sizeof(npm))) {
+                        for (int j = i; j < data; j++) {
+                            stu[j] = stu[j + 1];
+                        }
+                        data = data - 1;
+                        printf("Action complete! Data deleted.\n"); 
+                        found = true;
+                    } else {
+                        found = false;
                     }
                 }
 
-                if (found > 0) {
-                    for (int i = d; i < data; i++) {
-                        if (i + 1 > data) {
-                            stu[i] = stu[i + 1];
-                        }
-                    }
-                    data = data - 1;
-                    printf("Action complete! Data deleted.\n");                 
-                } else {
+                if (!found) {
                     printf("Data not found!\n");
                 }
                 break;
@@ -194,7 +183,8 @@ int main(int argc, char const *argv[])
         printf("Please choose one : \n");
         printf("\t[1] Read all data\n");
         printf("\t[2] Read data based on NPM\n");
-        printf("\t[3] Read sorted by NPM\n");
+        printf("\t[3] Read bubble-sorted data by NPM (Ascending)\n");
+        printf("\t[4] Read select-sorted data by NPM (Ascending)\n");
         printf("Your command : "); scanf("%d", &x);
         return x;
     }
@@ -208,4 +198,54 @@ int main(int argc, char const *argv[])
         printf("\tFaculty    : %s\n", stu.faculty);
         printf("\tSemester   : %d\n", stu.semester);
         printf("\tAge        : %d\n", stu.age);
+    }
+
+    void bubble_sort (Students stu[], int length) {
+        Students loc_stu[length], tmp_stu;
+        for (int i = 0; i < length; i++) {
+            loc_stu[i] = stu[i];
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < length; j++) {
+                if (strcmp(loc_stu[j-1].npm, loc_stu[j].npm) > 0) {
+                    tmp_stu = loc_stu[j-1];
+                    loc_stu[j-1] = loc_stu[j];
+                    loc_stu[j] = tmp_stu;
+                }
+                                    
+            }
+        }
+                            
+        for (int i = 0; i < length; i++) {
+            printf("Data number %i :\n", i+1);
+            rd_data(loc_stu[i]);
+            printf("\n");
+        }
+    }
+
+    void select_sort (Students stu[], int length) {
+        Students tmp_stu, loc_stu[length];
+
+        for (int i = 0; i < length; i++) {
+            loc_stu[i] = stu[i];
+        }
+        
+        for (int i = 0; i < length - 1; i++) {
+            int p = i;
+            for (int j = i + 1; j < length; j++) {
+                if (strcmp(loc_stu[j].npm, loc_stu[p].npm) < 0) {
+                    p = j;
+                }
+            }
+            tmp_stu    = loc_stu[p];
+            loc_stu[p] = loc_stu[i];
+            loc_stu[i] = tmp_stu;
+        }
+
+        for (int i = 0; i < length; i++) {
+            printf("Data number %i :\n", i+1);
+            rd_data(loc_stu[i]);
+            printf("\n");
+        }
     }
